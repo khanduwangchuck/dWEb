fc795c10bc63cdfebaac09937c7d4821bb069b9b
cdoe with regdenkhandu <regdenkhandu563@gmai.com>
2022-05-30 12:32:24 +0600

    first commits

---
+++ main.py
@@ -0,0 +1,5 @@
+
+#print(" I am boy, I love you,\n\t Thersre are all dame habbit:\n \t\t Hellwo eworld ian good\n\t\t i am in d=same lie \n i am in same line")
+#printing python version
+import sys
+#print("The version of this PYthon is :" +sys.version)
